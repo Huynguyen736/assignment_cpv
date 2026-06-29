16b4bebaebe0d1a9a7060487937180d6ea39cc9f
Nguyen Nhat Huy <nguyenhuy07032006@gmail.com>
2026-06-29 14:05:25 +0700

    feat: implement local knn for improve confident detection

--- assignment_face/models/test.py
+++ assignment_face/models/test.py
@@ -6,6 +6,9 @@ print(data.files)
 
 descriptors = data["descriptors"]
 student_ids = data["student_ids"]
+image_paths = data["image_paths"]
+grid_size = data["grid_size"]
+lbp_variant = data["lbp_variant"]
 
 print(descriptors.shape)
 print(student_ids)
